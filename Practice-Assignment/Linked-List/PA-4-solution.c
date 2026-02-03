b86e1ff2ada5ae2040c83fdbf68d56d6cdd7e8ff
Amit Kumar Singh <61898886+diaries404@users.noreply.github.com>
2026-02-03 21:28:50 +0530

    Update PA-4-solution.c

--- Practice-Assignment/Linked-List/PA-4-solution.c
+++ Practice-Assignment/Linked-List/PA-4-solution.c
@@ -1,1 +1,27 @@
+// function for find out the length of the given linkedlist.
+int getLength(struct Node* head) {
 
+    int length = 0;
+    
+    // find length of linked list
+    while (head) {
+        length++;
+        head = head->next;
+    }
+
+    return length;
+}
+// function for finding out the middle element.
+int getMiddle(struct Node* head) {
+
+    // finding length of the linked list
+    int length = getLength(head);
+
+    // traverse till we reach half of the length
+    int midIndex = length / 2;
+    while (midIndex--) {
+        head = head->next;
+    }
+
+    return head->data;
+}
